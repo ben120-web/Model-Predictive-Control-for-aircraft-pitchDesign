6f90a9d2cbe90c7f56e2b7409d8520e66d911413
Ben Russell <benrussell120@hotmail.co.uk>
2024-01-14 20:52:51 +0000

    Using polytop to get invariant set

--- MPC_Controller.py
+++ MPC_Controller.py
@@ -30,6 +30,43 @@ N = 10
 
 ## Compute the maximal invariant set with polytope.
 # This set defines the safe operating region for the system.
+def next_polytope(poly_j, poly_kappa_f, a_closed_loop):
+    """
+    Function:
+        calculate the next polytope
+    Inputs:
+        poly_j        : the previous polytope
+        poly_kappa_f  : the initial poly kappa f
+        a_closed_loop : \dot{x}=Ax+B@Ku
+    Returns:
+        pc.Polytope(Hnext, bnext) : the next polytope
+    """
+    (Hj, bj) = (poly_j.A, poly_j.b)
+    (Hkf, bkf) = (poly_kappa_f.A, poly_kappa_f.b)
+    Hnext = np.vstack((Hkf, Hj @ a_closed_loop))
+    bnext = np.concatenate((bkf, bj))
+    return pc.Polytope(Hnext, bnext)
+
+
+def determine_maximal_invariant_set(poly_kappa_f, a_closed_loop):
+    """
+    Function:
+        determine the maximal invariant set
+    Inputs:
+        poly_kappa_f  : the initial poly kappa f
+        a_closed_loop : \dot{x}=Ax+B@Ku
+    Returns:
+        inv_next      : the maximal invariant set
+    """
+    
+    inv_prev = poly_kappa_f  # use the initial poly kappa f as the previous one before the loop
+    keep_running = True
+    while keep_running:  # loop to calculate the maximal set
+        inv_next = next_polytope(inv_prev, poly_kappa_f, a_closed_loop)  # calculate the next one
+        inv_next = pc.reduce(inv_next)
+        keep_running = inv_next >= inv_prev  # if next one >= previous one, continue
+        inv_prev = inv_next
+    return inv_next
 
 ## Impliment and test controller.
 
